4358f14532fa4c2a1fcdeb3b269304aee3660098
Eyüb Salih Özdemir <eyupsalih776@gmail.com>
2022-03-30 12:20:38 +0300

    adding project files

---
+++ launcher/build/generated/source/buildConfig/release/com/SalihOzdemir/Klotski/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.SalihOzdemir.Klotski;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.SalihOzdemir.Klotski";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
